0b77ac7fa700f199cbc39e3c584a930dafc3848b
Levi Pshemish <133231470+levipshemish@users.noreply.github.com>
2025-04-30 12:01:24 -0400

    Update NewProducts.js with key

--- components/NewProducts.js
+++ components/NewProducts.js
@@ -6,7 +6,7 @@ export default function NewProducts({products}) {
           <h2 className="text-2xl py-3 px-2 bg-[#FAFAFF] text-black">New Arrivals</h2>
          <div className="grid grid-cols-4 gap-4 bg-white w-[93vw] mx-auto">
         {products?.length > 0 && products.map(product => (              
-              <div>
+              <div key={product._id}>
                     <ProductBox {...product}/>
                 </div>
             ))}
